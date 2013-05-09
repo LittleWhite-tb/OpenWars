124536418a7b656fab459fcc8708b7c057afef28
lw.demoscene <lw.demoscene@c386e1a7-df23-1227-0626-52ef9c75e855>
2013-05-09 16:23:54 +0000

    Added factory to create an engine without having dependencies

---
+++ branches/NEngine/src/NEngineFactory.cpp
@@ -0,0 +1,40 @@
+#ifndef DOXYGEN_IGNORE_TAG
+/**
+OpenAWars is an open turn by turn strategic game aiming to recreate the feeling of advance (famicon) wars (c)
+Copyright (C) 2010-2013  Alexandre LAURENT
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+website: http://code.google.com/p/openawars/
+e-mail: lw.demoscene@gmail.com
+**/
+#endif
+
+#include "NEngine/NEngineFactory.hpp"
+
+#include "Native/SDL/SDL_Engine.h"
+
+NE::NEngine* NE::NEngineFactory::createNEngine(NE::EngineType type)
+{
+    switch (type)
+    {
+        case SDL_ENGINE:
+            return new NE::SDL_Engine();
+            break;
+    }
+    
+    return NULL;
+}
+
